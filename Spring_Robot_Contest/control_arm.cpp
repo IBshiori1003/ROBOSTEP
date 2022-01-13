07ef20902757df03d74a0edaedab49d4d1294e08
Akihisa_Watanabe <akihisa@ruri.waseda.jp>
2022-01-14 00:15:40 +0900

    Update control_arm.cpp

--- Spring_Robot_Contest/control_arm.cpp
+++ Spring_Robot_Contest/control_arm.cpp
@@ -1,14 +1,15 @@
 #include "mbed.h"
 
 #define PWM_T 0.1 //PWM周期：100ms
-PwmOut PWM_OUT(PB_6); //超音波センサモジュールのTriger端子に入力する信号
+PwmOut PWM_TRIGER(PB_6); //超音波センサモジュールのTriger端子に入力する信号
 
 
-InterraptIn GET_PWM(PC_7); //割り込み入力端子の設定．マイコンから出力したPWM信号をD9端子から取り込む．  
+InterruptIn GET_PWM(PC_7); //割り込み入力端子の設定．マイコンから出力したPWM信号をD9端子から取り込む．  
 
 Timer ActiveTime; //タイマー計測用変数
 
 unsigned int Count;
+
 unsigned char Flag;
 double dist;
 
@@ -25,23 +26,25 @@ void FallEcho(){
     Flag = 1; //フラグのリセット
 }
  
-void main(){
+int main(){
     GET_PWM.rise(&RiseEcho);
     GET_PWM.fall(&FallEcho);
     
-    PWM_OUT.perido(PWM_T);
-    PWM_OUT.write(0.01f);//duty比:1%
+    PWM_TRIGER.period(PWM_T);
+    PWM_TRIGER.write(0.01f);//duty比:1%
     
     Count=0;
     Flag= 0;
+    int threshold=100;
     ActiveTime.reset();
     
     while(1){
         if (Flag==1){
+            if (dist > threshold){
             /*ここにモータを動かす処理を書く
             distに計測値(距離)が格納されている．前の値との差分に閾値を設けるのがいいのかな．．．
-            */
-            
+            */        
+            }
         }
     }
 }
